e47e8d315f51a995e5eab1c837b32e6b8d4dfca5
Sverre Stikbakke <sverre.stikbakke@gmail.com>
2019-03-10 19:08:08 +0100

    test-72

--- src/type_conflict_output.py
+++ src/type_conflict_output.py
@@ -1,5 +1,7 @@
 a = 1
 b = 'B'
 
-print(a + b)  # TypeError:
-              # unsupported operand type(s) for +: 'int' and 'str'
+print(a + b)  
+
+# TypeError:
+# unsupported operand type(s) for +: 'int' and 'str'
